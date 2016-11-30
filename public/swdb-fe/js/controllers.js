2d0061d8f455d692d20e2b00f6d20434193c2137
R. Ellis <rellis6022@gmail.com>
2016-11-30 14:15:52 -0800

    Fixed client code to used called port intead of REST port in configuration. Fixed up Gruntfile.

--- public/swdb-fe/js/controllers.js
+++ public/swdb-fe/js/controllers.js
@@ -1,12 +1,14 @@
 var appController = angular.module('appController', ['datatables','ngAnimate','ngSanitize','ui.bootstrap']);
 
-appController.run(['$rootScope','$route','$http','$routeParams', function($rootScope,$route,$http,$routeParams) {
+appController.run(['$rootScope','$route','$http','$routeParams','$location', function($rootScope,$route,$http,$routeParams,$location) {
 	$rootScope.$on("$routeChangeSuccess", function(currentRoute, previousRoute){
     //Change page title, based on Route information
     $rootScope.title = $route.current.title;
   });
-	$http.get("http://localhost:3005/swdbserv/v1/config").success(function(data) {
+	$http.get($location.protocol()+"://"+
+		$location.host()+":"+$location.port()+"/swdbserv/v1/config").success(function(data) {
 		$rootScope.props = data;
+		$rootScope.clientProps = {"port": $location.port()};
 	});
 
 }]);
@@ -25,7 +27,8 @@ function WithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q, $rootScop
 	var vm = this;
 	vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
 		var defer = $q.defer();
-		$http.get("http://localhost:"+$rootScope.props.restPort+"/swdbserv/v1").then(function(result) {
+		console.log("port got:"+$rootScope.clientProps.port);
+		$http.get("http://localhost:"+$rootScope.clientProps.port+"/swdbserv/v1").then(function(result) {
 			defer.resolve(result.data);
 		});
 		return defer.promise;
@@ -49,7 +52,7 @@ function WithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q, $rootScop
 appController.controller('DetailsController', ['$scope', '$http','$routeParams', '$rootScope', function ($scope, $http, $routeParams, $rootScope) {
 
 	//update document fields with existing data
-	$http.get("http://localhost:"+$rootScope.props.restPort+"/swdbserv/v1/"+$routeParams.itemId).success(function(data) {
+	$http.get("http://localhost:"+$rootScope.clientProps.port+"/swdbserv/v1/"+$routeParams.itemId).success(function(data) {
 		$scope.formData = data;
 		$scope.whichItem = $routeParams.itemId;
 	});
@@ -84,7 +87,7 @@ appController.controller('NewController', ['$scope', '$http','$rootScope', funct
 		if ($scope.inputForm.$valid){
 			$http({
 				method: 'POST',
-				url: "http://localhost:"+$rootScope.props.restPort+"/swdbserv/v1",
+				url: "http://localhost:"+$rootScope.clientProps.port+"/swdbserv/v1",
 				data: $scope.formData,
 				headers: { 'Content-Type': 'application/json' }
 			})
@@ -174,7 +177,7 @@ appController.controller('UpdateController', ['$scope', '$http', '$routeParams',
 			delete $scope.formData.__v;
 			$http({
 				method: 'PUT',
-				url: "http://localhost:"+$rootScope.props.restPort+"/swdbserv/v1/"+$scope.formData._id,
+				url: "http://localhost:"+$rootScope.clientProps.port+"/swdbserv/v1/"+$scope.formData._id,
 				data: $scope.formData,
 				headers: { 'Content-Type': 'application/json' }
 			})
@@ -251,7 +254,7 @@ appController.controller('UpdateController', ['$scope', '$http', '$routeParams',
 	getEnums();
 
 	//update document fields with existing data
-	$http.get("http://localhost:"+$rootScope.props.restPort+"/swdbserv/v1/"+$routeParams.itemId).success(function(data) {
+	$http.get("http://localhost:"+$rootScope.clientProps.port+"/swdbserv/v1/"+$routeParams.itemId).success(function(data) {
 		$scope.formData = data;
 		$scope.whichItem = $routeParams.itemId;
 
