8dc755fecc9e1d29a007e59664826ef751842e50
Dylan Maxwell <maxwelld@frib.msu.edu>
2017-03-30 18:24:25 -0400

    Add missing source and configuration files

---
+++ src/app/index.ts
@@ -0,0 +1,137 @@
+
+import fs = require('fs');
+import path = require('path');
+
+import bodyparser = require('body-parser');
+import express = require('express');
+import favicon = require('serve-favicon');
+import morgan = require('morgan');
+import session = require('express-session');
+
+import monitor_routes = require('./routes/monitor');
+
+// error interface
+interface StatusError extends Error {
+  status?: number
+}
+
+// application configuration
+interface IAppCfg {
+  port?: string,
+  session_life?: number,
+  session_secret?: string
+};
+
+// application singleton
+let app: express.Application;
+
+// application logging
+let log = console.log;
+let warn = console.warn;
+let error = console.error;
+
+// read configuration file in JSON format
+async function readConfigFile(name: string): Promise<object> {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(path.resolve(path.resolve(__dirname, '../config', name)), 'utf8', function (err, data) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(JSON.parse(data));
+    });
+  });
+};
+
+// Asynchronously start the application
+async function start(): Promise<express.Application> {
+  if (app) {
+    return app;
+  }
+  
+  log('Application starting');
+
+  app = express();
+  let env = app.get('env')
+
+  let cfg: IAppCfg = await readConfigFile('app.json')
+
+  app.set('port', cfg.port || 3000);
+
+  // view engine configuration
+  app.set('views', path.resolve(__dirname, '../views'));
+  app.set('view engine', 'pug');
+
+  // favicon configuration
+  app.use(favicon(path.resolve(__dirname, '../public', 'favicon.ico')));
+
+  // morgan configuration
+  morgan.token('remote-user', function (req) {
+    if (req.session && req.session.userid) {
+      return req.session.userid;
+    } else {
+      return 'unknown';
+    }
+  });
+
+  if (env === 'production') {
+    app.use(morgan('short'))
+  } else {
+    app.use(morgan('dev'));
+  }
+
+  // body-parser configuration
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({
+    extended: false
+  }));
+
+  // session configuration
+  app.use(session({
+    store: new session.MemoryStore(),
+    resave: false,
+    saveUninitialized: false,
+    secret: cfg.session_secret || 'secret',
+    cookie: {
+      maxAge: cfg.session_life || 28800000,
+    },
+    logErrors: function (err) {
+      //log.error(err);
+    }
+  }));
+
+  app.use(express.static(path.resolve(__dirname, '../public')));
+  app.use(express.static(path.resolve(__dirname, '../bower_components')));
+
+  app.use('/monitor', monitor_routes);
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    let err: StatusError = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // error handlers
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+
+  log('Application started');
+
+  return app;
+};
+
+
+// Asynchronously start the application
+async function stop(): Promise<void> {
+  log('Application stopping');
+  log('Application stopped');
+  return;
+}
+
+export { start, stop, log, warn, error };
---
+++ src/app/shared/monitor.ts
@@ -0,0 +1,222 @@
+/*
+ * A common module for monitoring the status of an application.
+ */
+import process = require('process');
+
+interface ComponentStatus {
+  status: string,
+  date: Date,
+  name: string,
+  message: string
+}
+
+interface MonitorStatus {
+  status: string,
+  uptime: number,
+  components: [ComponentStatus]
+}
+
+// Utilities
+
+function toMB(memory: number): string {
+  return Math.round(memory/1048576).toString() + 'MB';
+}
+
+function toPCT(load: number): string {
+  return load.toPrecision(2);
+}
+
+// Monitor process CPU load //
+let load = 0.0;
+
+function getLoad(): number {
+  return load;
+}
+
+let loadStatus: ComponentStatus;
+
+function getLoadStatus(): ComponentStatus {
+  return loadStatus;
+}
+
+let loadLimit = 0.5;
+
+function getLoadLimit(): number {
+  return loadLimit;
+};
+
+function setLoadLimit(limit: number) {
+  if (limit > 0.0) {
+    loadLimit = limit;
+    updateLoadStatus();
+  }
+};
+
+function updateLoadStatus() {
+   if (load < getLoadLimit()) {
+    loadStatus = {
+      status: 'OK',
+      date: new Date(),
+      name: 'Load',
+      message: toPCT(load) + ' < ' + toPCT(getLoadLimit())
+    };
+  } else {
+    loadStatus = {
+      status: 'ERROR',
+      date: new Date(),
+      name: 'Load',
+      message: toPCT(load) + ' >= ' + toPCT(getLoadLimit())
+    };
+  }
+};
+
+let last = process.cpuUsage();
+
+let loadInterval = (10*60*1000) // 10 minutes //
+
+function monitorLoad() {
+  let next = process.cpuUsage();
+  let user = (next.system-last.system)/1000;
+  let system = (next.system-last.system)/1000;
+  let load = (user+system) / loadInterval;
+  last = next;
+  updateLoadStatus();
+}
+
+// Initialize load status //
+updateLoadStatus();
+
+// TODO: should clear interval //
+setInterval(monitorLoad, loadInterval);
+
+// Monitor application memory //
+let memory = process.memoryUsage().heapTotal;
+
+function getMemory(): number {
+  return memory;
+}
+
+let memoryStatus: ComponentStatus;
+
+function getMemoryStatus(): ComponentStatus {
+  return memoryStatus;
+}
+
+let memoryLimit = (2*1024*1024*1024); // 2 Gigabyte //
+
+function getMemoryLimit(): number {
+  return memoryLimit;
+};
+
+function setMemoryLimit(limit: number) {
+  if (limit > 0.0) {
+    memoryLimit = limit;
+    updateMemoryStatus();
+  }
+};
+
+function updateMemoryStatus() {
+ if (memory < getMemoryLimit()) {
+    memoryStatus = {
+      status: 'OK',
+      date: new Date(),
+      name: 'Memory',
+      message: toMB(memory) + ' < ' + toMB(getMemoryLimit())
+    };
+  } else {
+    memoryStatus = {
+      status: 'ERROR',
+      date: new Date(),
+      name: 'Memory',
+      message: toMB(memory) + " >= " + toMB(getMemoryLimit())
+    };
+  }
+};
+
+let memoryInterval = (1*60*1000) // 1 minutes //
+
+function monitorMemory() {
+  memory = process.memoryUsage().heapTotal;
+  updateMemoryStatus();
+};
+
+// Initialize memory status //
+updateMemoryStatus();
+
+// TODO: should clear interval
+setInterval(monitorMemory, memoryInterval);
+
+// Custom status //
+
+let components = <[ComponentStatus]>[];
+
+function setOk(name: string, message?: string) {
+  for (let comp of components) {
+    if (name === comp.name) {
+      comp.status = 'OK';
+      comp.message = message || 'OK';
+      return;
+    }
+  }
+  components.push({
+    status: 'OK',
+    date: new Date(),
+    name: name,
+    message: message || 'OK'
+  });
+}
+
+function setError(name: string, message?: string) {
+  for (let comp of components) {
+    if (name === comp.name) {
+      comp.status = 'ERROR';
+      comp.message = message || 'ERROR';
+      return;
+    }
+  }
+  components.push({
+    status: 'ERROR',
+    date: new Date(),
+    name: name,
+    message: message || 'ERROR'
+  });
+}
+
+function getStatus(): MonitorStatus {
+  let status = {
+    status: 'OK',
+    uptime: process.uptime(),
+    components: <[ComponentStatus]>[]
+  }
+
+  status.components.push(memoryStatus);
+  if (memoryStatus.status !== 'OK') {
+    status.status = 'ERROR';
+  }
+
+  status.components.push(loadStatus);
+  if (loadStatus.status !== 'OK') {
+    status.status = 'ERROR';
+  }
+
+  for (let comp of components) {
+    status.components.push(comp);
+    if (comp.status !== 'OK') {
+      status.status = 'ERROR'
+    }
+  }
+
+  return status;
+};
+
+export {
+  getLoad,
+  getLoadLimit,
+  setLoadLimit,
+  getLoadStatus,
+  getMemory,
+  getMemoryLimit,
+  setMemoryLimit,
+  getMemoryStatus,
+  getStatus
+};
