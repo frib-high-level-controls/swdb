2304362eed788ee15a8e9d28fd7e55d3316e3bf3
R. Ellis <rellis6022@gmail.com>
2017-05-16 10:19:52 -0700

    Added areas enum, reinstated software tests

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
-//require('./store/swdb-spec.js');
-//require('./store/swdb-new.js');
+require('./store/swdb-spec.js');
+require('./store/swdb-new.js');
 require('./store/inst-spec.js');
 require('./store/inst-list.js');
 require('./store/inst-new.js');
