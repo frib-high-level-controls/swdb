8cef5b9d3439ba9956d31eb57bf3e8321125755c
R. Ellis <rellis6022@gmail.com>
2017-06-15 19:49:48 -0700

    Adde a new sw test data to test name/version/branch. Also changed swdb to index name/ver/branch as unique.

---
+++ test/misc/popTestSwData.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+from pymongo import MongoClient
+import json
+from pprint import pprint
+
+with open("../../config/properties.json") as prop_file:
+    prop_data = json.load(prop_file)
+
+with open("./swTestData.json") as data_file:
+    data = json.load(data_file)
+
+pprint(prop_data)
+pprint(data)
+
+print "WARNING; This program will be monkeying with the DB defined in the"
+print "properties file: "+prop_data["mongodbUrl"]
+
+raw_input("Hit ENTER to continue (ctl-c to exit)...")
+
+client = MongoClient(prop_data["mongodbUrl"])
+db = client[prop_data["mongodbUrl"].split("/")[-1]]
+#coll = client['swNamesProp']
+
+result = db.swdbCollection.insert_many(data)
