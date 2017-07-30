0856c17ad9b82bce1914b510946066b04574efee
FRIB Controls deployment <deployer@swdb-dev.nscl.msu.edu>
2017-07-30 07:22:55 -0400

    Modified test db populators to use ssl/novalidate

--- test/misc/popTestSwData.py
+++ test/misc/popTestSwData.py
@@ -4,6 +4,8 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import json
 from pprint import pprint
+import re
+import ssl
 
 with open("../../config/properties.json") as prop_file:
     prop_data = json.load(prop_file)
@@ -25,8 +27,28 @@ print "properties file: "+prop_data["mongodbUrl"]
 
 raw_input("Hit ENTER to continue (ctl-c to exit)...")
 
-client = MongoClient(prop_data["mongodbUrl"])
-db = client[prop_data["mongodbUrl"].split("/")[-1]]
+
+
+# Detect ssl params, remove from string and set in client
+newmongodbUrl = prop_data["mongodbUrl"]
+
+pattern = re.compile(r'&ssl=true&sslValidate=false')
+if pattern.search(newmongodbUrl):
+	newmongodbUrl = re.sub(pattern,"",newmongodbUrl,1)
+
+client = MongoClient(newmongodbUrl, ssl = True, ssl_cert_reqs = ssl.CERT_NONE)
+
+# find the db name to use
+db = client[newmongodbUrl.split("authSource=")[-1]]
+#coll = client['swNamesProp']
+
+#result =db.swNamesProp.insert_many(data)
+
+
+
+
+#client = MongoClient(prop_data["mongodbUrl"])
+#db = client[prop_data["mongodbUrl"].split("/")[-1]]
 #coll = client['swNamesProp']
 
 result = db.swdbCollection.insert_many(data)
