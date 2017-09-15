59f79ccf68475f612ed0a8cd08205c4b61aced75
Dylan Maxwell <maxwelld@frib.msu.edu>
2017-09-15 13:50:55 -0400

    Add shared auth utilities with CAS/FORG support

---
+++ src/app/shared/auth.ts
@@ -0,0 +1,200 @@
+/**
+ * Shared authentication and authorization utilities.
+ */
+import * as dbg from 'debug';
+import * as util from 'util';
+
+import * as express from 'express';
+
+import * as handlers from './handlers';
+
+type Request = express.Request;
+type Response = express.Response;
+type NextFunction = express.NextFunction;
+type RequestHandler = express.RequestHandler;
+
+export interface IUser {
+  [key: string]: {};
+};
+
+/**
+ * The IProvider interface is based on the Passport API.
+ * See: http://passportjs.org
+ */
+export interface IProvider {
+
+  authenticate(options?: any): RequestHandler;
+
+  logout(req: Request): void;
+
+  getUser(req: Request): IUser | undefined;
+
+  getUsername(req: Request): string | undefined;
+
+  getRoles(req: Request): string[] | undefined;
+
+  hasRole(req: Request, role: string | string[]): boolean;
+
+  hasAnyRole(req: Request, role: string | string[]): boolean;
+};
+
+const debug = dbg('webapp:auth');
+
+
+export abstract class AbstractProvider implements IProvider {
+
+  public abstract authenticate(options?: any): RequestHandler;
+
+  public abstract logout(req: Request): void;
+
+  public abstract getUser(req: Request): IUser | undefined;
+
+  public abstract getUsername(req: Request): string | undefined;
+
+  public abstract getRoles(req: Request): string[] | undefined;
+
+  public hasRole(req: Request, role: string | string[]): boolean {
+    const userRoles = this.getRoles(req);
+    if (!userRoles) {
+      return false;
+    }
+
+    const roles = new Set<string>();
+    if (Array.isArray(role)) {
+      for (let r of role) {
+        roles.add(r.toUpperCase());
+      }
+    } else {
+      roles.add(role.toUpperCase());
+    }
+
+    for (let userRole of userRoles) {
+      if (!roles.has(userRole.toUpperCase())) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  public hasAnyRole(req: express.Request, role: string | string[]): boolean {
+    const userRoles = this.getRoles(req);
+    if (!userRoles) {
+      return false;
+    }
+
+    const roles = new Set<string>();
+    if (Array.isArray(role)) {
+      for (let r of role) {
+        roles.add(r.toUpperCase());
+      }
+    } else {
+      roles.add(role.toUpperCase());
+    }
+
+    for (let userRole of userRoles) {
+      if (roles.has(userRole.toUpperCase())) {
+        return true;
+      }
+    }
+    return false;
+  };
+};
+
+
+class NullProvider extends AbstractProvider {
+
+  public authenticate(options: object): express.RequestHandler {
+    return (req: Request, res: Response, next: NextFunction) => {
+      res.status(handlers.HttpStatus.FORBIDDEN);
+      res.send('not authorized');
+    };
+  };
+
+  public logout(req: Request): void {
+    return;
+  }
+
+  public getUser(req: Request): IUser | undefined {
+    return;
+  }
+
+  public getUsername(req: Request): string | undefined {
+    return;
+  }
+
+  public getRoles(req: Request): string[] | undefined {
+    return;
+  }
+};
+
+function sendUnauthorized(req: Request, res: Response, type: string, realm: string, msg?: string) {
+  res.header('WWW-Authenticate', util.format('%s realm="%s"', type, realm));
+  res.status(handlers.HttpStatus.UNAUTHORIZED);
+  res.send(msg ? msg : 'not authenticated');
+};
+
+function sendForbidden(req: Request, res: Response, msg?: string) {
+  res.status(handlers.HttpStatus.FORBIDDEN);
+  res.send(msg ? msg : 'not authorized');
+};
+
+
+const nullProvider = new NullProvider();
+
+let defaultProvider: IProvider | undefined;
+
+export function getProvider(): IProvider {
+  return defaultProvider || nullProvider;
+};
+
+export function setProvider(provider: IProvider) {
+  defaultProvider = provider;
+};
+
+export function getUsername(req: Request): string | undefined {
+  return getProvider().getUsername(req);
+};
+
+export function hasRole(req: Request, role: string | string[]): boolean {
+  return getProvider().hasRole(req, role);
+};
+
+export function hasAnyRole(req: Request, role: string | string[]): boolean {
+  return getProvider().hasAnyRole(req, role);
+};
+
+// TODO: Consider making this a function that returns a request handler.
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+  const username = getUsername(req);
+  if (!username) {
+    const redirectUrl = '/login?bounce=' + encodeURIComponent(req.originalUrl);
+    debug('Request NOT authenticted: Redirect: %s', redirectUrl);
+    res.redirect(redirectUrl);
+    return;
+  }
+  next();
+};
+
+export function ensureHasRole(role: string | string[]): RequestHandler {
+  return (req, res, next) => {
+    ensureAuthenticated(req, res, (err: any) => {
+      if (!hasRole(req, role)) {
+        sendForbidden(req, res);
+        return;
+      }
+      next();
+    });
+  };
+};
+
+export function ensureHasAnyRole(role: string | string[]): RequestHandler {
+  return (req, res, next) => {
+    ensureAuthenticated(req, res, (err: any) => {
+      if (!hasAnyRole(req, role)) {
+        sendForbidden(req, res);
+        return;
+      }
+      next();
+    });
+  };
+};
---
+++ src/app/shared/forgapi.ts
@@ -0,0 +1,158 @@
+/**
+ * Client for FORG API
+ */
+import * as util from 'util';
+
+import * as request from 'request';
+
+import * as status from '../shared/status';
+
+// Types for API v1
+export interface User {
+  uid: string;
+  fullname: string;
+  lastname: string;
+  firstname: string;
+  // email: string,
+  // office: string,
+  // phone: string,
+  // mobile: string,
+  roles: string[];
+};
+
+export interface SearchUser {
+  uid: string;
+  fullname: string;
+  lastname: string;
+  firstname: string;
+  role: string;
+}
+
+export type GroupType = 'LAB' | 'UNIT' | 'DIV' | 'DEPT' | 'GROUP' | 'TEAM' | 'AREA' | 'UNKNOWN';
+
+export interface ApiGroup {
+  uid: string;
+  name: string;
+  fullname: string;
+  leader: string;
+  source: string;
+  type: GroupType;
+};
+
+export interface SearchGroup {
+  uid: string;
+  name: string;
+  fullname: string;
+  source: string;
+  type: GroupType;
+  role: string;
+};
+
+export interface IClient {
+  findUser(uid: string): Promise<User | undefined>;
+};
+
+export interface ClientOptions {
+  url: string;
+  agentOptions?: any;
+}
+
+export class Client implements IClient {
+
+  protected options: ClientOptions;
+
+  constructor(options: string | ClientOptions) {
+    if (typeof options === 'string') {
+      options = { url: options };
+    }
+    this.options = options;
+  };
+
+  public async findUser(uid: string): Promise<User | undefined> {
+    const reqopts = this.getBaseOptions();
+    reqopts.uri += '/api/v1/users/' + encodeURIComponent(uid);
+
+    return new Promise<User | undefined>((resolve, reject) => {
+      request.get(reqopts, (err, res, body) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (res.statusCode === 401) {
+          resolve();
+          return;
+        }
+        if (res.statusCode !== 200) {
+          reject(new Error(util.format('FORG API request: %s, status: %s', reqopts.uri, res.statusCode)));
+          return;
+        }
+        try {
+          let user: User = JSON.parse(body);
+          // TODO: JSON schema validate!
+          resolve(user);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  };
+
+  public async findUsers(): Promise<User[]> {
+    const reqopts = this.getBaseOptions();
+    reqopts.uri += '/api/v1/users';
+
+    return new Promise<User[]>((resolve, reject) => {
+      request.get(reqopts, (err, res, body) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (res.statusCode !== 200) {
+          reject(new Error(util.format('FORG API request: %s, status: %s', reqopts.uri, res.statusCode)));
+          return;
+        }
+        try {
+          let users: User[] = JSON.parse(body);
+          // TODO: JSON schema validate!
+          resolve(users);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  };
+
+  public async findStatus(): Promise<status.ApiApplicationStatus> {
+    const reqopts = this.getBaseOptions();
+    reqopts.uri += '/status/json';
+
+    return new Promise<status.ApiApplicationStatus>((resolve, reject) => {
+      request.get(reqopts, (err, res, body) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        try {
+          let status: status.ApiApplicationStatus = JSON.parse(body);
+          // TODO: JSON schema validate!
+          resolve(status);
+        } catch (err) {
+          reject(err);
+        }
+      });
+    });
+  };
+
+  protected getBaseOptions(): (request.UriOptions & request.CoreOptions) {
+    if (!this.options.url) {
+      throw new Error('FORG API base URL not specified');
+    }
+    return {
+      uri: this.options.url,
+      headers: {
+        Accept: 'application/json',
+      },
+      agentOptions: this.options.agentOptions || {},
+    };
+  };
+};
