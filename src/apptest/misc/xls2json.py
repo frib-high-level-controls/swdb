ab72c8c630e5dc309ddc887acbfccfd2370a1d45
Robert Ellis <rellis6022@gmail.com>
2017-09-17 02:00:11 -0700

    Fixed up xls translator for multiple sheets and UI issues

--- src/apptest/misc/xls2json.py
+++ src/apptest/misc/xls2json.py
@@ -11,62 +11,71 @@ wb = xlrd.open_workbook('/home/deployer/Downloads/Software_Configuration_DRR01-0
 sheet = wb.sheet_by_index(2)
 array = []
 instArray = []
-
 # keep track of keys to ignore dupes.
 swKeyList = {}
 instKeyList = {}
 
-for nrow in range(1, sheet.nrows):
-  row = OrderedDict()
-  cols = sheet.row_values(nrow)
-  if cols[0] != "":
-    keyStr = cols[2]+"-"+str(cols[5])
-    if not swKeyList.get(keyStr):
-      # store the _id for this key for future lookups
-      swKeyList[keyStr] = format(nrow, "024x")
-      row["swName"] = cols[2]
-      row["desc"] = cols[1]
-      row["status"] = "Ready for Beam"
-      row["statusDate"] = time.strftime("%m/%d/%Y")
-      row["version"] = str(cols[5])
-      row["area"] = cols[6]
-      row["owner"] = cols[7]
-      row["engineer"] = cols[8]
-      row["levelOfCare"] = cols[9]
-      row["platforms"] = cols[10]
-      row["revisionControl"] = cols[11]
-      row["revisionControlLoc"] = cols[12]
-      row["_id"] = format(nrow, "024x")
+id = 0
 
-      print str(nrow)+":Adding sw row "+json.dumps(row)
-      array.append(row)
-    else:
-      print "Found existing swName:" + cols[2] + " version:" + str(cols[5]) + " skipping."
+for sheet in wb.sheets():
+  print "looking at sheet " + sheet.name
+  for nrow in range(1, sheet.nrows):
+    id=id+1
+    row = OrderedDict()
+    cols = sheet.row_values(nrow)
+    if cols[0] != "":
+      keyStr = cols[2]+"-"+str(cols[5])
+      if not swKeyList.get(keyStr):
+        # store the _id for this key for future lookups
+        swKeyList[keyStr] = format(id, "024x")
+        row["swName"] = cols[2]
+        row["desc"] = cols[1]
+        row["status"] = "Ready for Installation"
+        # row["statusDate"] = time.strftime("%m/%d/%Y")
+        #Note sw status date is set below from the installation status date
+        row["version"] = str(cols[5])
+        row["area"] = cols[6]
+        row["owner"] = cols[7]
+        row["engineer"] = cols[8]
+        row["levelOfCare"] = cols[9]
+        row["platforms"] = cols[10]
+        row["revisionControl"] = cols[11]
+        row["revisionControlLoc"] = cols[12]
+        row["_id"] = format(id, "024x")
 
-    for host in cols[3].split(','):
-      instKeyStr = host+"-"+swKeyList[keyStr]
-      # check that this installation is not already present
-      if not instKeyList.get(instKeyStr):
-        instKeyList[instKeyStr] = True
-        instRow = OrderedDict()
-        instRow["host"] = host
-        instRow["area"] = cols[6]
-        instRow["status"] = cols[4]
-        # instRow["statusDate"] = time.strftime("%m/%d/%Y", cols[15])
-        vvDate = sheet.cell_value(nrow, 15)
-        if vvDate:
-          # instRow["statusDate"] = time.strftime("%m/%d/%Y", cols[15])
-          vvDatetime = datetime.datetime(*xlrd.xldate_as_tuple(sheet.cell_value(nrow, 15), wb.datemode))
-          # print "DATE TEST: %s" % vvDatetime
-          instRow["statusDate"] = vvDatetime.strftime("%m/%d/%Y")
-
-        instRow["vvResultsLoc"] = cols[14]
-        instRow["software"] = swKeyList[keyStr]
-
-        print "  "+str(nrow)+":Adding inst row "+json.dumps(instRow)
-        instArray.append(instRow)
+        print "\n" + str(nrow)+":Adding sw row "+json.dumps(row)
+        array.append(row)
       else:
-        print "Found existing installation host:" + host + " sw:" + swKeyList[keyStr] + " skipping."
+        print "Found existing swName:" + cols[2] + " version:" + str(cols[5]) + " skipping."
+
+      for host in cols[3].split(','):
+        instKeyStr = host+"-"+swKeyList[keyStr]
+        # check that this installation is not already present
+        if not instKeyList.get(instKeyStr):
+          instKeyList[instKeyStr] = True
+          instRow = OrderedDict()
+          instRow["host"] = host
+          instRow["area"] = cols[6]
+          instRow["status"] = cols[4]
+          # instRow["statusDate"] = time.strftime("%m/%d/%Y", cols[15])
+          vvDate = sheet.cell_value(nrow, 15)
+          if vvDate:
+            # instRow["statusDate"] = time.strftime("%m/%d/%Y", cols[15])
+            vvDatetime = datetime.datetime(*xlrd.xldate_as_tuple(sheet.cell_value(nrow, 15), wb.datemode))
+            # print "DATE TEST: %s" % vvDatetime
+            instRow["statusDate"] = vvDatetime.strftime("%m/%d/%Y")
+
+            # sw status date gets set from installation status date
+            row["statusDate"] = vvDatetime.strftime("%m/%d/%Y")
+
+          instRow["vvResultsLoc"] = cols[14]
+          instRow["software"] = swKeyList[keyStr]
+          instRow["drrs"] = sheet.name
+
+          print "  "+str(nrow)+":Adding inst row "+json.dumps(instRow)
+          instArray.append(instRow)
+        else:
+          print "Found existing installation host:" + host + " sw:" + swKeyList[keyStr] + " skipping."
 
 json_data = json.dumps(array, indent=2)
 inst_json_data = json.dumps(instArray, indent=2)
