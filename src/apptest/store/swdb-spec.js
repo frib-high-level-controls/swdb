6db711469e0e584c9d2e759b2bba79a8897b1ffd
Robert Ellis <rellis6022@gmail.com>
2018-01-27 07:54:18 -0800

    Updated swdb-spec, swdb-list, and swdb-new for passwport-cas test framework.

--- src/apptest/store/swdb-spec.js
+++ src/apptest/store/swdb-spec.js
@@ -39,18 +39,16 @@ describe("app", function() {
 
   before("login as test user", function(done){
     supertest
-    // .get("/testlogin?username=testuser&password=testuserpasswd")
-    // .expect(200)
-    // .end(function(err,res){
-    //   Cookies = res.headers['set-cookie'].pop().split(';')[0];
-    //   done();
-    // });
-    .get("/caslogin?username=testuser&password=testuserpasswd")
-    .expect(200)
+    .get("/caslogin")
+    .auth('ellisr', 'Pa5w0rd')
+    .expect(302)
     .end(function(err,res){
-      // Cookies = res.headers['set-cookie'].pop().split(';')[0];
-      debug('test login cookies: ' + JSON.stringify(res.body));
-      done();
+      if (err) done(err);
+      else {
+        Cookies = res.headers['set-cookie'].pop().split(';')[0];
+        debug('test login cookies: ' + JSON.stringify(Cookies));
+        done();
+      }
     });
   });
 
