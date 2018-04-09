fcaff09123ec4e9a85e578a9f8eb3837c85d9520
Dylan Maxwell <maxwelld@frib.msu.edu>
2018-04-09 16:09:06 -0400

    Cleanup formatting of Gruntfile (indentation, quotes)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,60 +1,60 @@
 module.exports = function(grunt) {
-    'use strict';
+  'use strict';
 
-    grunt.initConfig({
-        ts: {
-            app: {
-                tsconfig: {
-                   tsconfig: './src/app',
-                   passThrough: true,
-                },
-                // The additional flags specified below seems like it should be equivalent
-                // to using the outDir option, but when the outDir option is used then the
-                // Typescript compiler fails for find the source files (grunt-ts v5.5.1).
-                //outDir: './app',
-                options: {
-                    additionalFlags: '--outDir ./app'
-                },
-            },
-            apptest: {
-                tsconfig: {
-                   tsconfig: './src/apptest',
-                   passThrough: true,
-                },
-                options: {
-                    additionalFlags: '--outDir ./test'
-                },
-            }
+  grunt.initConfig({
+    ts: {
+      app: {
+        tsconfig: {
+          tsconfig: './src/app',
+          passThrough: true,
         },
-        tslint: {
-            options: {
-                configuration: "tslint.json",
-                // If set to true, tslint errors will be reported, but not fail the task 
-                // If set to false, tslint errors will be reported, and the task will fail 
-                force: false,
-                fix: false
-            },
-            files: {
-                src: [
-                    "src/**/*.ts"
-                ]
-            }
+        // The additional flags specified below seems like it should be equivalent
+        // to using the outDir option, but when the outDir option is used then the
+        // Typescript compiler fails for find the source files (grunt-ts v5.5.1).
+        //outDir: './app',
+        options: {
+          additionalFlags: '--outDir ./app'
         },
-        clean: {
-            app: [ './app' ],
-            test: [ './test' ],
-        }
+      },
+      apptest: {
+        tsconfig: {
+          tsconfig: './src/apptest',
+          passThrough: true,
+        },
+        options: {
+          additionalFlags: '--outDir ./test'
+        },
+      },
+    },
+    tslint: {
+      options: {
+        configuration: 'tslint.json',
+          // If set to true, tslint errors will be reported, but not fail the task 
+          // If set to false, tslint errors will be reported, and the task will fail 
+          force: false,
+          fix: false
+        },
+        files: {
+          src: [
+            'src/**/*.ts'
+          ],
+        },
+      },
+      clean: {
+        app: [ './app' ],
+        test: [ './test' ],
+      }
     });
 
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks("grunt-tslint");
-    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask("default", [
-        "ts:app"
+    grunt.registerTask('default', [
+      'ts:app'
     ]);
 
     grunt.registerTask('lint', [
-        'tslint',
+      'tslint',
     ]);
-}
+};
