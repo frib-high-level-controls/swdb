b7721c67d8259d1a023b78accddfc96e899e4070
Dylan Maxwell <maxwelld@frib.msu.edu>
2018-06-01 15:10:48 -0400

    Convert SW list view controller to TypeScript

---
+++ src/web/ts/angular-datatables.d.ts
@@ -0,0 +1,33 @@
+/**
+ * Partial type definiations for angular-datatables v0.6.X 
+ * 
+ * For more information: http://l-lin.github.io/angular-datatables/archives/#!/welcome
+ */
+import * as angular from 'angular';
+
+declare module 'angular' {
+
+    namespace datatables {
+        interface DTOptionsBuilderService {
+            fromFnPromise<T>(fn: () => PromiseLike<T>): DTOptionsBuilderService;
+            
+            withBootstrap(): DTOptionsBuilderService;
+
+            withPaginationType(type: 'full_numbers'): DTOptionsBuilderService;
+
+            withDOM(dom: string): DTOptionsBuilderService;
+        }
+
+        interface DTColumnBuilderService {
+            newColumn(target: string): DTColumnBuilder;
+        }
+
+        interface DTColumnBuilder {
+            withTitle(title: string): DTColumnBuilder;
+
+            withOption(opt: string, val: any): DTColumnBuilder;
+
+            renderWith(fn: (data: any, type: string, full: any, meta: {}) => string): DTColumnBuilder;
+        }
+    }
+}
--- src/web/ts/ctlr-swdb.ts
+++ src/web/ts/ctlr-swdb.ts
@@ -2,33 +2,89 @@
  * This is the top-level controller for swdb
  */
 
-var appController = angular.module('appController', ['datatables', 'datatables.bootstrap', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ngCookies', 'ui.select']);
+interface IAppRootScopeService extends ng.IRootScopeService {
+  title?: string;
+}
 
-appController.run(['$rootScope', '$route', '$http', '$routeParams', '$location', 'configService', function ($rootScope, $route, $http, $routeParams, $location, configService, slotService) {
+interface IAppRoute extends ng.route.IRoute {
+  title: string;
+}
 
-  $rootScope.$on("$routeChangeSuccess", function (currentRoute, previousRoute) {
-    //Change page title, based on Route information
-    $rootScope.title = $route.current.title;
+interface IConfigProps {
+  webUrl: string;
+}
 
-  });
-}]);
+interface IConfigService {
+  getConfig(): IConfigProps;
+}
+
+interface IUserService {
+  getUser(): {};
+}
+
+let appController = angular.module('appController', [
+  'datatables',
+  'datatables.bootstrap',
+  'ngAnimate',
+  'ngSanitize',
+  'ui.bootstrap',
+  'ngCookies',
+  'ui.select',
+]);
+
+appController.run(['$rootScope', '$route', '$http', '$routeParams', '$location', 'configService',
+  function (
+    $rootScope: IAppRootScopeService,
+    $route: ng.route.IRouteService,
+    $http: ng.IHttpService,
+    $routeParams: ng.route.IRouteParamsService,
+    $location: ng.ILocationService,
+    configService: {},
+    slotService: {},
+  ) {
+    $rootScope.$on('$routeChangeSuccess', function (event, currentRoute: IAppRoute) {
+      // Change page title, based on Route information
+      $rootScope.title = currentRoute.title;
+
+    });
+  },
+]);
 
 // expose system status to all controllers via service.
 appController.factory('StatusService', function () {
   return {
-    dbConnected: 'false'
+    dbConnected: 'false',
   };
 });
 
-appController.controller('ListController', ListPromiseCtrl);
-function ListPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q, $scope, $cookies, $window, configService, userService) {
+interface IListControllerScope extends ng.IScope {
+  session: {
+    user?: {};
+  };
+  props: IConfigProps;
+  usrBtnTxt?: string;
+  usrBtnClk(): void;
+};
 
+appController.controller('ListController', ListPromiseCtrl);
+function ListPromiseCtrl(
+  this: { dtOptions: {}, dtColumns: {} },
+  DTOptionsBuilder: ng.datatables.DTOptionsBuilderService,
+  DTColumnBuilder: ng.datatables.DTColumnBuilderService,
+  $http: ng.IHttpService,
+  $q: ng.IQService,
+  $scope: IListControllerScope,
+  $cookies: {},
+  $window: ng.IWindowService,
+  configService: IConfigService,
+  userService: IUserService,
+) {
   $scope.$watch(function () {
     return $scope.session;
   }, function () {
     // prep for login button
     if ($scope.session && $scope.session.user) {
-      $scope.usrBtnTxt = "";
+      $scope.usrBtnTxt = '';
     } else {
       $scope.usrBtnTxt = 'Log in';
     }
@@ -45,12 +101,12 @@ function ListPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q, $scope, $
   // get intitialization info
   $scope.props = configService.getConfig();
   $scope.session = userService.getUser();
-  var vm = this;
+  let vm = this;
   vm.dtOptions = DTOptionsBuilder
     .fromFnPromise(function () {
-      var defer = $q.defer();
-      let url = basePath + "/api/v1/swdb";
-      $http.get(url).then(function (result) {
+      let defer = $q.defer();
+      let url = basePath + '/api/v1/swdb';
+      $http.get<{data: {}}>(url).then(function (result) {
         defer.resolve(result.data);
       });
       return defer.promise;
@@ -71,51 +127,54 @@ function ListPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q, $scope, $
     DTColumnBuilder.newColumn('version')
       .withTitle('Version').withOption('defaultContent', ''),
     DTColumnBuilder.newColumn('owner')
-      .withTitle('Owner').withOption('defaultContent', ""),
+      .withTitle('Owner').withOption('defaultContent', ''),
     DTColumnBuilder.newColumn('engineer')
-      .withTitle('Engineer').withOption('defaultContent', ""),
+      .withTitle('Engineer').withOption('defaultContent', ''),
     DTColumnBuilder.newColumn('status')
-      .withTitle('Status').withOption('defaultContent', ""),
+      .withTitle('Status').withOption('defaultContent', ''),
     DTColumnBuilder.newColumn('statusDate')
       .withTitle('Status date (m/d/y)')
       .renderWith(function (data, type, full, meta) {
         let thisDate = new Date(full.statusDate);
-        let month = thisDate.getMonth()+1;
+        let month = thisDate.getMonth() + 1;
         let day = thisDate.getDate();
         let year = thisDate.getFullYear();
         return month + '/' + day + '/' + year;
-      })
+      }),
   ];
 
   angular.element('#swdbList').on('init.dt', function (event, loadedDT) {
     // wait for the init event from the datatable
     // (then it is done loading)
     // Handle multiple init notifications
-    let id = '#' + event.target.id;
+    let id = '#' + $(event.target).attr('id');
     let num = $(id).find('thead').find('tr').length;
-    if (num == 1) {
-      var table = $(id).DataTable();
+    if (num === 1) {
+      let table = $(id).DataTable();
       let tr = $('<tr/>').appendTo($(id).find('thead'));
 
       // Apply the search
       table.columns().eq(0).each(function (colIdx) {
         let th = $('<th></th>').appendTo(tr);
+        let column = table.column(colIdx);
         if (true) {
           // append column search with id derived from column init data
-          th.append('<input class="swdbTableHeaderSearch" id="' + table.settings().init().aoColumns[colIdx].mData + "Srch" + '" type="text" placeholder="' + (table.column(colIdx).placeholder || '')
+          th.append('<input class="swdbTableHeaderSearch" id="' + column.dataSrc() + 'Srch'
+            + '" type="text" placeholder="' + ((table.column(colIdx) as any).placeholder || '')
             + '" style="width:80%;" autocomplete="off">');
-          th.on('keyup', 'input', function () {
-            let elem = this; // aids type inference to avoid cast
+          th.on('keyup', 'input', function (evt) {
+            let elem = evt.target;
             if (elem instanceof HTMLInputElement) {
               table.column(colIdx).search(elem.value).draw();
             }
           });
 
           // Now apply filter routines to each column
-          $('input', table.column(colIdx).header()).on('keyup change', function () {
+          $('input', table.column(colIdx).header()).on('keyup change', function (evt) {
+            let v = $(evt.target).val();
             table
               .column(colIdx)
-              .search(this.value)
+              .search(v ? String(v) : '')
               .draw();
           });
         }
@@ -123,4 +182,3 @@ function ListPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q, $scope, $
     }
   });
 }
-
