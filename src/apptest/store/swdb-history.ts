92164009f2b68d55497bc9e18c46199fb9203c0a
Robert Ellis <rellis6022@gmail.com>
2018-06-12 12:36:31 -0700

    Converted swdb-history to TS

---
+++ src/apptest/store/swdb-history.ts
@@ -0,0 +1,114 @@
+import server = require('../../app/server');
+import Chai = require('chai');
+import chaiAsPromised = require('chai-as-promised');
+import Supertest = require('supertest');
+import Be = require('../../app/lib/Db');
+import TestTools = require('./TestTools');
+// const circJSON = require('circular-json');
+
+import CommonTools = require('../../app/lib/CommonTools');
+let ctools = new CommonTools.CommonTools();
+let props: CommonTools.IProps;
+props = ctools.getConfiguration();
+import dbg = require('debug');
+const debug = dbg('swdb:swdb-history-tests');
+let app;
+let supertest: Supertest.SuperTest<Supertest.Test>;
+let testTools = new TestTools.TestTools();
+Chai.use(chaiAsPromised);
+let expect = Chai.expect;
+
+let Cookies: string;
+//
+describe('History tests suite', () => {
+  before('Prep DB', async () => {
+    app = await server.start();
+    supertest = Supertest(app);
+    debug('Prep DB');
+    await testTools.clearTestCollections(debug);
+    await testTools.loadTestCollectionsStandard(debug, props.test.swTestDataFile, props.test.instTestDataFile);
+  });
+
+  after('clear db', async () => {
+    debug('Clear DB');
+    // clear the test collection
+    await testTools.clearTestCollections(debug);
+    await server.stop();
+  });
+
+  // describe("Login and perform history tests", function () {
+  let wrapper = { origId: null };
+
+  before('login as test user', function (this, done) {
+    this.timeout(8000);
+    supertest
+    .get('/login')
+    .auth(props.test.username, props.test.password)
+    .timeout(8000)
+    .expect(302)
+    .end((err, res) => {
+      if (err) {
+        done(err);
+      } else {
+        Cookies = res.header['set-cookie'].pop().split(';')[0];
+        debug('test login cookies: ' + JSON.stringify(Cookies));
+        done();
+      }
+    });
+  });
+
+  it('Has the blank history', async () => {
+    let cursor = Be.Db.swDoc.db.collections.history.find();
+    let count: number = await cursor.count();
+    debug('Found ' + count + ' history items');
+    expect(count).to.equal(0);
+  });
+
+  it('Post a new record with correct history', (done) => {
+    supertest
+      .post('/api/v1/swdb')
+      .set('Accept', 'application/json')
+      .set('Cookie', Cookies)
+      .send({ swName: 'Test Record', owner: 'Owner 1000', engineer: 'Engineer 1000',
+       levelOfCare: 'LOW', status: 'DEVEL', statusDate: 'date 1000' })
+      .end(async (err: Error, result: Supertest.Response) => {
+        // get record id from the returned location and find records that match
+        let id = result.header.location.split(/\//).pop();
+        wrapper.origId = id;
+        debug('Got id ' + id);
+        let canonObj = {
+          swName: 'Test Record', owner: 'Owner 1000', engineer: 'Engineer 1000',
+          levelOfCare: 'LOW', status: 'DEVEL', statusDate: new Date('date 1000'),
+        };
+        try {
+          expect(await testTools.checkHistory(debug, canonObj, id)).to.equal('History record matches');
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+  });
+
+  it('Update a software record with correct history', (done) => {
+    supertest
+      .put('/api/v1/swdb/' + wrapper.origId)
+      .set('Accept', 'application/json')
+      .set('Cookie', Cookies)
+      .send( { owner: 'New test owner' } )
+      .end(async (err: Error, result: Supertest.Response) => {
+        // get record id from the returned location and find records that match
+        let id = result.header.location.split(/\//).pop();
+        wrapper.origId = id;
+        debug('Got id ' + id);
+        let canonObj = {
+            owner: 'New test owner',
+          };
+        try {
+          expect(await testTools.checkHistory(debug, canonObj, id)).to.equal('History record matches');
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+  });
+});
