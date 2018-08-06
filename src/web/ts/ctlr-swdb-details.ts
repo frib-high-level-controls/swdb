2e46dbefa53807b71ac9fc9fadacc820b5fa0d72
Robert Ellis <rellis6022@gmail.com>
2018-08-06 08:00:56 -0700

    Lint fixes.

--- src/web/ts/ctlr-swdb-details.ts
+++ src/web/ts/ctlr-swdb-details.ts
@@ -56,55 +56,55 @@ function DetailsPromiseCtrl(
   userService: IUserService,
   recService: IRecService,
 ) {
-  $scope.$watch(function () {
+  $scope.$watch(() => {
     return $scope.session;
-  }, function () {
+  }, () => {
     // prep for login button
     if ($scope.session && $scope.session.user) {
-      $scope.usrBtnTxt = "";
+      $scope.usrBtnTxt = '';
     } else {
       $scope.usrBtnTxt = 'Log in';
     }
   }, true);
 
-  $scope.usrBtnClk = function () {
+  $scope.usrBtnClk = () => {
     if ($scope.session.user) {
       $window.location.href = $scope.props.webUrl + 'logout';
     } else {
       $window.location.href = $scope.props.webUrl + 'login';
     }
   };
-  
-  $scope.bumpVerBtnClk = function () {
-    // set the recService and then transfer to the new 
+
+  $scope.bumpVerBtnClk = () => {
+    // set the recService and then transfer to the new
     recService.setRec({
       updateRecID: $routeParams.itemId,
       formData: $scope.formData,
     });
-    $location.path("/new");
+    $location.path('/new');
   };
 
-  let mkHistTable = function (data: IHistory[]){
+  const mkHistTable = (data: IHistory[]) => {
     let table = '<table id="histTable" class="swdbHistTable">';
-    data.map(function(elem, idx, arr){
-      table = table.concat('<tr class="swdbHistTr"><td class="swdbHistTdSection">'+
+    data.map((elem, idx, arr) => {
+      table = table.concat('<tr class="swdbHistTr"><td class="swdbHistTdSection">' +
         new Date(elem.at) + '</td><td class="swdbHistTdSection">' + elem.by + '</td></tr>');
-      elem.paths.map(function(pathElem, patIdx, pathArr){
+      elem.paths.map((pathElem, patIdx, pathArr) => {
         table = table.concat('<tr class="swdbHistTr"><td class="swdbHistTd">' +
           pathElem.name + '</td><td class="swdbHistTd">' + pathElem.value + '</td></tr>');
       });
     });
-    table = table.concat("</table>");
+    table = table.concat('</table>');
     return table;
   };
 
   $scope.props = configService.getConfig();
   $scope.session = userService.getUser();
-  //update document fields with existing data
-  let url = basePath + "/api/v1/swdb/" + $routeParams.itemId;
+  // update document fields with existing data
+  let url = basePath + '/api/v1/swdb/' + $routeParams.itemId;
 
-  swService.refreshSwList().then(function () {
-    let data = swService.getSwById($routeParams.itemId);
+  swService.refreshSwList().then(() => {
+    const data = swService.getSwById($routeParams.itemId);
     $scope.formData = data;
     // convert level of care key to value
     $scope.levelOfCareDisplay = $scope.props.LevelOfCareEnum[data.levelOfCare];
@@ -114,19 +114,19 @@ function DetailsPromiseCtrl(
     }
     // format dates for display
     if (data.statusDate) {
-      let thisDate = new Date(data.statusDate);
-      let month = thisDate.getMonth()+1;
-      let day = thisDate.getDate();
-      let year = thisDate.getFullYear();
+      const thisDate = new Date(data.statusDate);
+      const month = thisDate.getMonth() + 1;
+      const day = thisDate.getDate();
+      const year = thisDate.getFullYear();
       $scope.formData.statusDate =  month + '/' + day + '/' + year;
     }
   });
 
   // get history
-  url = basePath + "/api/v1/swdb/hist/" + $routeParams.itemId;
-  $http.get(url).then(function (data) {
+  url = basePath + '/api/v1/swdb/hist/' + $routeParams.itemId;
+  $http.get(url).then((data) => {
     $scope.rawHistory = data.data as IHistory[];
-    $scope.rawHistory.map(function(elem: IHistory, idx: number, arr: IHistory[]) {
+    $scope.rawHistory.map((elem: IHistory, idx: number, arr: IHistory[]) => {
       elem.isCollapsed = true;
     });
     $scope.isHistCollapsed = false;
