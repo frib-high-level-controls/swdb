a447b647c93e06b44cbe8445872a6af1ddb4376d
Robert Ellis <rellis6022@gmail.com>
2018-08-08 08:26:30 -0700

    Lint fixes.

--- src/web/ts/ctlr-swdb-new.ts
+++ src/web/ts/ctlr-swdb-new.ts
@@ -74,36 +74,34 @@ function NewPromiseCtrl(
   recService: IRecService,
 ) {
 
-  $scope.$watch(function () {
+  $scope.$watch( () => {
     return $scope.session;
-  }, function () {
+  },  () => {
     // prep for login button
     if ($scope.session && $scope.session.user) {
-      $scope.usrBtnTxt = "";
+      $scope.usrBtnTxt = '';
     } else {
       $scope.usrBtnTxt = 'Log in';
     }
   }, true);
 
-  $scope.usrBtnClk = function () {
+  $scope.usrBtnClk =  () => {
     if ($scope.session.user) {
-      // $window.location.href = $scope.props.webUrl + 'logout';
       $location.path($scope.props.webUrl + 'logout');
     } else {
-      // $window.location.href = $scope.props.webUrl + 'login';
       $location.path($scope.props.webUrl + 'login');
     }
   };
 
-  $scope.bckBtnClk = function () {
-    $location.path("/list");
+  $scope.bckBtnClk =  () => {
+    $location.path('/list');
   };
 
-  $scope.datePicker = (function () {
-    var method: any = {};
+  $scope.datePicker = ( () => {
+    const method: any = {};
     method.instances = [];
 
-    method.open = function ($event: any, instance: any) {
+    method.open =  ($event: any, instance: any) => {
       $event.preventDefault();
       $event.stopPropagation();
       method.instances[instance] = true;
@@ -111,40 +109,40 @@ function NewPromiseCtrl(
 
     method.options = {
       'show-weeks': false,
-      startingDay: 0
+      'startingDay': 0,
     };
 
     method.format = 'M!/d!/yyyy';
     return method;
-  }());
+  })();
 
-  $scope.newItem = function (event: {currentTarget: HTMLInputElement}) {
-    var parts = event.currentTarget.id.split('.');
+  $scope.newItem =  (event: {currentTarget: HTMLInputElement}) => {
+    const parts = event.currentTarget.id.split('.');
     if (parts[1] === 'vvProcLoc') {
-      if ($scope.formData.vvProcLoc){
-      $scope.formData.vvProcLoc.push("");
+      if ($scope.formData.vvProcLoc) {
+      $scope.formData.vvProcLoc.push('');
       }
     } else if (parts[1] === 'vvResultsLoc') {
-      if ($scope.formData.vvResultsLoc){
-      $scope.formData.vvResultsLoc.push("");
+      if ($scope.formData.vvResultsLoc) {
+      $scope.formData.vvResultsLoc.push('');
     }
     }
   };
 
-  $scope.removeItem = function (event) {
-    var parts = event.currentTarget.id.split('.');
+  $scope.removeItem =  (event) => {
+    const parts = event.currentTarget.id.split('.');
     if (parts[1] === 'vvProcLoc') {
-      if ($scope.formData.vvProcLoc){
+      if ($scope.formData.vvProcLoc) {
         $scope.formData.vvProcLoc.splice(Number(parts[2]), 1);
       }
     } else if (parts[1] === 'vvResultsLoc') {
-      if ($scope.formData.vvResultsLoc){
+      if ($scope.formData.vvResultsLoc) {
         $scope.formData.vvResultsLoc.splice(Number(parts[2]), 1);
       }
     }
   };
 
-  $scope.processForm = function () {
+  $scope.processForm =  () => {
     // Prep any selected owner
     if ($scope.ownerSelected.item) {
       $scope.formData.owner = $scope.ownerSelected.item.uid;
@@ -157,97 +155,83 @@ function NewPromiseCtrl(
     $scope.formData.statusDate = $scope.statusDateDisplay.toISOString();
 
     // convert enum values to keys
-    $scope.formData.levelOfCare = Object.keys($scope.props.LevelOfCareEnum).filter( 
-      function (item) { 
+    $scope.formData.levelOfCare = Object.keys($scope.props.LevelOfCareEnum).filter(
+       (item) => {
         return $scope.levelOfCareDisplay === $scope.props.LevelOfCareEnum[item];
       })[0];
-    $scope.formData.status = Object.keys($scope.props.StatusEnum).filter( 
-      function (item) { 
+    $scope.formData.status = Object.keys($scope.props.StatusEnum).filter(
+       (item) => {
         return $scope.statusDisplay === $scope.props.StatusEnum[item];
       })[0];
-    $scope.formData.versionControl = Object.keys($scope.props.RcsEnum).filter( 
-      function (item) { 
+    $scope.formData.versionControl = Object.keys($scope.props.RcsEnum).filter(
+       (item) => {
         return $scope.versionControlDisplay === $scope.props.RcsEnum[item];
       })[0];
 
-    if (!$scope.formData.version) {
-    }
     if ($scope.inputForm.$valid) {
-      let url = basePath + "/api/v1/swdb";
+      const url = basePath + '/api/v1/swdb';
       $http({
         method: 'POST',
         url: url,
         data: $scope.formData,
-        headers: { 'Content-Type': 'application/json' }
+        headers: { 'Content-Type': 'application/json' },
       })
         .then(function success(response) {
-          $scope.swdbParams.formStatus = "Document posted";
+          $scope.swdbParams.formStatus = 'Document posted';
           $scope.swdbParams.formShowErr = false;
           $scope.swdbParams.formShowStatus = true;
-          let headers = response.headers();
+          const headers = response.headers();
           // sw just updated, refresh the service list
           swService.refreshSwList();
           if (headers.location) {
             // if location header is present extract the id
-            let id = headers.location.split('/').pop();
+            const id = headers.location.split('/').pop();
             $location.path('/details/' + id);
           }
         }, function error(response) {
-          if (response.data.match(/^Validation errors: /g)){
+          if (response.data.match(/^Validation errors: /g)) {
             // unpack the validation errors and print the first
-            const parts = response.data.split('Validation errors: ')
+            const parts = response.data.split('Validation errors: ');
             const errors = JSON.parse(parts[1]);
-            $scope.swdbParams.formErr = "Error: " + errors[0].msg + " (" + response.status + ")";
+            $scope.swdbParams.formErr = 'Error: ' + errors[0].msg + ' (' + response.status + ')';
           } else {
-            $scope.swdbParams.formErr = "Error: " + JSON.stringify(response.data) + " (" + response.status + ")";
+            $scope.swdbParams.formErr = 'Error: ' + JSON.stringify(response.data) + ' (' + response.status + ')';
           }
           $scope.swdbParams.formShowStatus = false;
           $scope.swdbParams.formShowErr = true;
         });
     } else {
-      $scope.swdbParams.formErr = "Error: clear errors before submission";
+      $scope.swdbParams.formErr = 'Error: clear errors before submission';
       $scope.swdbParams.formShowStatus = false;
       $scope.swdbParams.formShowErr = true;
     }
   };
 
-  var getEnums = function () {
+  const getEnums = () => {
     $scope.levelOfCareDisplay = 'Low';
     $scope.formData.levelOfCare = 'LOW';
     $scope.statusDisplay = 'Development';
-    $scope.formData.status = "DEVEL";
-    $scope.formData.versionControl = "Other";
+    $scope.formData.status = 'DEVEL';
+    $scope.formData.versionControl = 'Other';
   };
-
-  // // set the engineer field to the selected user
-  // $scope.onEngineerSelect = function ($item, $model, $label) {
-  //   $scope.formData.engineer = $model.uid;
-  // };
-  // // set the owner field to the selected user
-  // $scope.onOwnerSelect = function ($item, $model, $label) {
-  //   $scope.formData.owner = $model.uid;
-  // };
 
   $scope.props = configService.getConfig();
   $scope.session = userService.getUser();
-  forgUserService.promise.then(function(){
+  forgUserService.promise.then(() => {
     $scope.forgUsersList = forgUserService.getUsers().data;
   });
 
-  forgGroupService.promise.then(function(){
+  forgGroupService.promise.then(() => {
     $scope.forgGroupsList = forgGroupService.getGroups().data;
   });
 
-  // $scope.itemArray = $scope.props.validSwNamesGUIList;
-
   // check our user session and redirect if needed
   if (!$scope.session.user) {
-    //go to cas
-    // $window.location.href = $scope.props.webUrl + 'login';
+    // go to cas
     $location.path($scope.props.webUrl + 'login');
   }
 
-  //initialize selected owner and engineer
+  // initialize selected owner and engineer
   $scope.ownerSelected = {item: undefined};
   $scope.engineerSelected = {item: undefined};
 
@@ -260,15 +244,15 @@ function NewPromiseCtrl(
   $scope.swdbParams = {
     formShowErr: false,
     formShowStatus: false,
-    formStatus: "",
-    formErr: ""
+    formStatus: '',
+    formErr: '',
   };
   getEnums();
 
   // expect recService to provide ID and formdata
-  let updateRec = recService.getRec();
+  const updateRec = recService.getRec();
   if (updateRec) {
-    let updateRedID = updateRec.updateRecID;
+    const updateRedID = updateRec.updateRecID;
     $scope.formData.swName = updateRec.formData.swName;
     $scope.formData.desc = updateRec.formData.desc;
     $scope.formData.owner = updateRec.formData.owner;
@@ -288,7 +272,7 @@ function NewPromiseCtrl(
     $scope.formData.vvProcLoc = updateRec.formData.vvProcLoc;
 
     $scope.formData.versionControl = updateRec.formData.versionControl;
-    if (updateRec.formData.versionControl){
+    if (updateRec.formData.versionControl) {
     $scope.versionControlDisplay = $scope.props.RcsEnum[updateRec.formData.versionControl];
     }
 
@@ -298,20 +282,20 @@ function NewPromiseCtrl(
     // got the new data, now clear the service for next time.
     recService.setRec(null);
     // convert the retreived record owner
-    forgGroupService.promise.then(function(){
+    forgGroupService.promise.then(() => {
       if ($scope.formData.owner) {
-      let thisOwner = [$scope.formData.owner];
-      let forgObjs = forgGroupService.groupUidsToObjects(thisOwner)[0];
+        const thisOwner = [$scope.formData.owner];
+        const forgObjs = forgGroupService.groupUidsToObjects(thisOwner)[0];
         $scope.ownerSelected = { item: forgObjs };
       }
-    })
+    });
     // convert the retreived record engineer
-    forgUserService.promise.then(function(){
+    forgUserService.promise.then(() => {
       if ($scope.formData.engineer) {
-      let thisEngineer = [$scope.formData.engineer];
-      let forgObjs = forgUserService.userUidsToObjects(thisEngineer)[0];
+        const thisEngineer = [$scope.formData.engineer];
+        const forgObjs = forgUserService.userUidsToObjects(thisEngineer)[0];
         $scope.engineerSelected = { item: forgObjs };
       }
-    })
-  };
+    });
+  }
 }
