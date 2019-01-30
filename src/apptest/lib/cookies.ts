56ba3479b6710439253048b332a680455ec41763
Dylan Maxwell <maxwelld@frib.msu.edu>
2019-01-30 15:57:40 -0500

    Update dependencies (app and web)

--- src/apptest/lib/cookies.ts
+++ src/apptest/lib/cookies.ts
@@ -2,11 +2,31 @@
  * Utilities to parse cookies from 'set-cookie' header in SuperTest Response.
  */
 import { assert } from 'chai';
-import { IWebDriverCookie as ICookie } from 'selenium-webdriver';
+import { IWebDriverCookie } from 'selenium-webdriver';
 import { Response } from 'supertest';
-import {Cookie as ToughCookie} from 'tough-cookie';
+import { Cookie as ToughCookie } from 'tough-cookie';
 
-interface ICookies<T = ICookie>  { [key: string]: T | undefined; }
+interface ICookies<T = IWebDriverCookie>  { [key: string]: T | undefined; }
+
+
+/**
+ * Convert a ToughCookie Cookie to a WebDriver Cookie!
+ */
+function toWebDriverCookie(c?: ToughCookie): IWebDriverCookie | undefined {
+  if (!c) {
+    return;
+  }
+  return {
+    name: c.key,
+    value: c.value,
+    path: c.path !== null ? c.path : undefined,
+    domain: c.domain !== null ? c.domain : undefined,
+    secure: c.secure,
+    httpOnly: c.httpOnly,
+    expiry: c.expires !== 'Infinity' ? c.expiryTime() : undefined,
+  };
+}
+
 
 /**
  * Parse all cookies from the 'set-cookie' header in the SuperTest Response.
@@ -14,20 +34,6 @@ interface ICookies<T = ICookie>  { [key: string]: T | undefined; }
 export function parseCookies(res: Response, raw?: false): ICookies;
 export function parseCookies(res: Response, raw: true): ICookies<ToughCookie>;
 export function parseCookies(res: Response, raw?: boolean): ICookies | ICookies<ToughCookie> {
-  function toCookie(c?: ToughCookie) {
-    if (!c) {
-      return;
-    }
-    return {
-      name: c.key,
-      value: c.value,
-      path: c.path,
-      domain: c.domain,
-      secure: c.secure,
-      httpOnly: c.httpOnly,
-      expiry: c.expiryTime(),
-    };
-  }
 
   const cookieHeader: {} = res.header['set-cookie'];
   if (!cookieHeader) {
@@ -55,13 +61,13 @@ export function parseCookies(res: Response, raw?: boolean): ICookies | ICookies<
   const cookies: ICookies = {};
   if (Array.isArray(cookieHeader)) {
     for (const cookie of cookieHeader) {
-      const c = toCookie(ToughCookie.parse(cookie));
+      const c = toWebDriverCookie(ToughCookie.parse(cookie));
       if (c) {
         cookies[c.name] = c;
       }
     }
   } else {
-    const c = toCookie(ToughCookie.parse(String(cookieHeader)));
+    const c = toWebDriverCookie(ToughCookie.parse(String(cookieHeader)));
     if (c) {
       cookies[c.name] = c;
     }
@@ -72,9 +78,9 @@ export function parseCookies(res: Response, raw?: boolean): ICookies | ICookies<
 /**
  * Parse the cookie with the specified name from the 'set-cookie' header in the SuperTest Response.
  */
-export function parseCookie(res: Response, name: string, raw?: false): ICookie;
+export function parseCookie(res: Response, name: string, raw?: false): IWebDriverCookie;
 export function parseCookie(res: Response, name: string, raw: true): ToughCookie;
-export function parseCookie(res: Response, name: string, raw?: boolean): ICookie | ToughCookie {
+export function parseCookie(res: Response, name: string, raw?: boolean): IWebDriverCookie | ToughCookie {
   if (raw) {
     const rawCookie = parseCookies(res, raw)[name];
     if (!rawCookie) {
